a07b60cb6c11cd450ed9b0a0efd03f96c7929dd0
Abinash Regmi <abinashregmi01234@gmail.com>
2025-08-27 14:08:43 +0000

    final

--- app/app.py
+++ app/app.py
@@ -4,17 +4,16 @@ import streamlit as st
 st.write("""
 # Simple Stock Price App
 
-Shown are the stock closing price and volume of Google!
+Shown are the stock closing price and volume of Apple Inc!
 
 """)
 
-# https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
-#define the ticker symbol
-tickerSymbol = 'GOOGL'
+
+tickerSymbol = 'AAPL'
 #get data on this ticker
 tickerData = yf.Ticker(tickerSymbol)
 #get the historical prices for this ticker
-tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+tickerDf = tickerData.history(start='2010-5-31', end='2020-5-31')
 # Open	High	Low	Close	Volume	Dividends	Stock Splits
 
 st.line_chart(tickerDf.Close)
